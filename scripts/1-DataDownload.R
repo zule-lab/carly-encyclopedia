8f9f9c32c760c1744706dda182a03671043dc0c9
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-11-01 14:02:01 -0400

    download arrondissements

--- scripts/1-DataDownload.R
+++ scripts/1-DataDownload.R
@@ -8,6 +8,10 @@ source('R/download_file.R')
 
 # Boundaries --------------------------------------------------------------
 
+# arrondissements
+arrondissements <- download_shp("https://donnees.montreal.ca/dataset/9797a946-9da8-41ec-8815-f6b276dec7e9/resource/e8bea324-044c-4544-bbd1-2e0d08a24216/download/limites-administratives-agglomeration-nad83.zip", 
+                                "input/arrondissements.zip")
+
 # montreal neighbourhoods
 quartiers <- download_shp("https://donnees.montreal.ca/dataset/c8f37ad6-16ff-4cdc-9e5a-e47898656fc9/resource/d342d18e-f710-4991-a259-0092bac3d62c/download/quartiers_sociologiques_2014.zip", 
                           "input/quartiers.zip")
@@ -29,6 +33,7 @@ can_2021 <- download_tif("https://observatoire.cmm.qc.ca/documents/carte/canope/
 # Census ------------------------------------------------------------------
 
 census_raw <- download_csv("https://www12.statcan.gc.ca/census-recensement/2021/dp-pd/prof/details/download-telecharger/comp/GetFile.cfm?Lang=E&FILETYPE=CSV&GEONO=006_Quebec")
+
 
 
 
